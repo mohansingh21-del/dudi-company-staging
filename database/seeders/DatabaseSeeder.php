c8fc1043868ed64323831278143f81808ed22b38
mohansingh21-del <noxx0497@gmail.com>
2026-08-27 14:06:28 +0530

    added leave type seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             AdminSeeder::class,
+            LeaveTypeSeeder::class,
         ]);
         
         // \App\Models\User::factory(10)->create();
